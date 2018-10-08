159626e665f5e4285a6e34c188bfe26c6a9674df
Joseba Carnicero <joseba.carnicero@alumni.mondragon.edu>
2018-10-08 18:23:44 +0200

    Commenting code - Conexion class commented - Controlador class commented

--- src/sociedad2/Conexion.java
+++ src/sociedad2/Conexion.java
@@ -1,36 +1,64 @@
+/** @file Conexion.java
+ *  @brief Class where we make the connection between the database and the java aplication
+ *  @authors
+ *  Name          | Surname        | Email                                |
+ *  ------------- | -------------- | ------------------------------------ |
+ *  Marcos          Azcarate         marcos.azcarate@alumni.mondragon.edu
+ *  @date 05/10/2018
+ */
+
+
+
+
+/** @brief Packages
+ */
 package sociedad2;
 
-
+/** @brief Libraries
+ */
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
-
-import com.mysql.jdbc.ResultSetMetaData;
-import com.mysql.jdbc.Statement;
-
+/**
+ * @brief Conexion class
+ */
 public class Conexion {
 	
-
+	/**
+	 * @brief Attributes
+	 */
 	Connection conex;
+	String usuario;
 	
-	public Conexion() {
+	/**
+	 * @brief The constructor of the class Conexion
+	 * @param usuario: The username of the person who is going to login
+	 * @param conex: It is the connection between the database and the java program
+ 	 */
+	public Conexion(String usuario) {
 		
-			conex = crearConexion();
+			this.usuario = usuario;
+			conex = crearConexionBD(usuario);
 		
 		}
 	
-public Connection crearConexion()
+	
+	/**
+	 * @brief Create the connection between the database and java
+	 * @param usuario: The username of the person who is going to login
+	 * @param con: It is the connection between the database and the java program
+	 * @param url: It is the location of our database
+ 	 */
+public Connection crearConexionBD(String usuario)
 	{
+		
 		Connection con=null;
 		try {
+			
 			Class.forName("com.mysql.jdbc.Driver");
 			String url="jdbc:mysql://localhost/sociedad";
-			con=DriverManager.getConnection(url,"root", null);
+			con=DriverManager.getConnection(url,usuario, "321");
 			return con;
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
--- src/sociedad2/Controlador.java
+++ src/sociedad2/Controlador.java
@@ -1,16 +1,43 @@
+/** @file Controlador.java
+ *  @brief Class in which we respond to user interactions
+ *  @authors
+ *  Name          | Surname        | Email                                |
+ *  ------------- | -------------- | ------------------------------------ |
+ *  Marcos          Azcarate         marcos.azcarate@alumni.mondragon.edu
+ *  @date 05/10/2018
+ */
+
+/** @brief Packages
+ */
 package sociedad2;
 
+/** @brief Libraries
+ */
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
 import com.mysql.jdbc.Connection;
 
+/**
+ * @brief Controlador class
+ */
 public class Controlador implements ActionListener {
 	
+	/**
+	 * @brief Attributes
+	 */
 	Vista vista;
 	Modelo modelo;
 	
+	
+	/**
+	 * @brief The constructor of the class Controlador
+	 * @param vista: Is the 'main' class of the project that represent the interface of the user
+	 * @param modelo: Handles the data, consulting the database
+ 	 */
 	public Controlador(Vista vista, Modelo modelo) {
 		
 		this.vista=vista;
@@ -18,17 +45,34 @@ public class Controlador implements ActionListener {
 		
 	}
 
+	/**
+	 * @brief Respond to the actions performed in the interface
+	 * @param con: It is the connection between the database and the java program
+	 * @param bai: It is the id of the registered user
+	 * @param e: Contains the action that the user want to do
+	 * @param baii: Contains the id of the last consumption made
+	 * @param i: Contains the id of the last registered member
+	 * @param a: Contains the id of the last registered product
+	 * @param dialogoProducto: Opens the interface where we can see the different products of the 'sociedad'
+	 * @param dialogoSocio: Opens the interface where we can see the different members of the 'sociedad'
+	 * @param dialogoVerTuHistorial: Opens the interface where we can see the different consumptions made in the 'sociedad'
+	 * @param consumiciones: Opens the interface where we can register different consumptions that we made in the 'sociedad'
+	 * @param dialogoSocio: Opens the interface where we can register a new member in the 'sociedad'
+	 * @param dialogoProducto: Opens the interface where we can register a new product in the 'sociedad'
+	 * @param dialogoBaja: Opens the interface where we can see the members written off
+	 * @param dialogoProdBaja: Opens the interface where we can see the products written off
+ 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		Connection con = (Connection) vista.getConexion().crearConexion();
+		Connection con = (Connection) vista.getConexion().crearConexionBD(vista.socio.getUsuario());
 
 		switch(e.getActionCommand())
 		{
 		case "VER PRODUCTOS":
 			try {
 				
-				DialogoVerProducto dialogoProducto = new DialogoVerProducto(vista,"Select * from productos where "
+				DialogoVerProductos dialogoProducto = new DialogoVerProductos(vista,"Select * from productos where "
 						+ " estado = 'activo' and categoriaID = 10","Productos", con);
 			} catch (SQLException e2) {
 				// TODO Auto-generated catch block
@@ -44,24 +88,46 @@ public class Controlador implements ActionListener {
 				e1.printStackTrace();
 			}
 			break;
-		case "VER TU HISTORIAL DE CONSUMICIONES":
+		case "CONSUMICIONES":
+			
+				int bai = vista.modelo.obtenerId("select * from consumiciones where socioId="+vista.getSocio().getSocioId(), con);
+				if(bai <= 1) {
+					JOptionPane.showMessageDialog(null, "No hay consumiciones registradas");
+				}else {
+					try {
+				DialogoConsumiciones dialogoVerTuHistorial =new DialogoConsumiciones(vista,"select * from consumiciones where socioId="+vista.getSocio().getSocioId()
+					+ " and "	,"Consumiciones",con, " ");
+					}catch (SQLException e3) {
+				// TODO Auto-generated catch block
+				e3.printStackTrace();
+					}
+				}
+			
 			break;
-		case "VER TODAS LAS CONSUMICIONES":
+		case "TODAS LAS CONSUMICIONES":
+			int baii = vista.modelo.obtenerId("select * from consumiciones", con);
+			if(baii <= 1) {
+				JOptionPane.showMessageDialog(null, "No hay consumiciones registradas");
+			}else {
+				try {
+					DialogoConsumiciones consumiciones = new DialogoConsumiciones(vista, "select * from consumiciones", "Consumiciones", con, " where");
+				} catch (SQLException e3) {
+					// TODO Auto-generated catch block
+					e3.printStackTrace();
+				}
+			}
 			break;
-		case "CAMBIAR INFO SOCIOS":
-			break;
-		case "CAMBIAR INFO PRODUCTOS":
-			break;
+		
 		case "REGISTRAR PEDIDO":
 			
 			modelo.comprobar();
 			
 			break;
-		case "REGISTRAR NUEVO SOCIO":
+		case "REGISTRAR SOCIO":
 			int i = modelo.obtenerId("Select max(socioID) from socios", con);
 			DialogoInsertarSocio dialogoSocio = new DialogoInsertarSocio(vista,i); 
 			break;
-		case "REGISTRAR NUEVO PRODUCTO":
+		case "REGISTRAR PRODUCTO":
 			int a = modelo.obtenerId("Select max(productoID) from productos", con);
 			DialogoInsertarProducto dialogoProducto = new DialogoInsertarProducto(this,a);
 			break;
@@ -91,6 +157,17 @@ public class Controlador implements ActionListener {
 				e1.printStackTrace();
 			}
 			
+			break;
+			
+		case "Cerrar Sesion": 
+			modelo.escribirData();
+			modelo.cerrarSesion(); 
+			Login log=new Login();
+			break;
+			
+		case "Pasar dia":
+			modelo.getFecha().pasarDia();
+			vista.getData().setText(modelo.getFecha().toString());
 			break;
 		}
 	}
